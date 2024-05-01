b7a86ef58ba5cba9d08aa9daddda4e19d1d43685
pat_mic <pat_mic@hotmail.com>
2024-05-01 12:14:12 -0500

    archivo hola en ok

--- Hola.java
+++ Hola.java
@@ -1,6 +1,7 @@
 public class Hola {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("que miras bobo..");
+        System.out.println("TAN LINDO Usted...");
     }
 }
